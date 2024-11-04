675a1eec59ac86da34ee470d5719117eb8483fb7
pakcdf <juseongb24@gmail.com>
2024-11-04 16:44:36 +0900

    motorola
    n

---
+++ js/script.js
@@ -0,0 +1,71 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//   // Smooth scrolling for anchor links
+//   document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+//     anchor.addEventListener("click", function (e) {
+//       e.preventDefault();
+//       document.querySelector(this.getAttribute("href")).scrollIntoView({
+//         behavior: "smooth",
+//       });
+//     });
+//   });
+
+//   // Newsletter form submission
+//   const newsletterForm = document.querySelector(".newsletter-form");
+//   if (newsletterForm) {
+//     newsletterForm.addEventListener("submit", function (e) {
+//       e.preventDefault();
+//       const email = this.querySelector('input[type="email"]').value;
+//       alert(`Thank you for subscribing with email: ${email}`);
+//       this.reset();
+//     });
+//   }
+// });
+
+// var swiper = new Swiper(".swiper", {
+//   slidesPerView: 3,
+//   direction: getDirection(),
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+//   on: {
+//     resize: function () {
+//       swiper.changeDirection(getDirection());
+//     },
+//   },
+// });
+
+// function getDirection() {
+//   var windowWidth = window.innerWidth;
+//   var direction = window.innerWidth <= 760 ? "vertical" : "horizontal";
+
+//   return direction;
+// }
+
+let swiper = new Swiper(".swiper", {
+  slidesPerView: 3,
+  direction: getDirection(),
+  autoplay: {
+    delay: 3000, // 3초마다 슬라이드 변경
+    disableOnInteraction: false, // 사용자 상호작용 후에도 자동 재생 계속
+  },
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  on: {
+    resize: function () {
+      swiper.changeDirection(getDirection());
+    },
+  },
+  spaceBetween: 50,
+});
+
+function getDirection() {
+  var windowWidth = window.innerWidth;
+  var direction = window.innerWidth <= 760 ? "vertical" : "horizontal";
+
+  return direction;
+}
+5;
